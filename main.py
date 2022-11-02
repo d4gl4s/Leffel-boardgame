b5e6bc4d515073d9a8ece297dfe31a636ad8587c
d4gl4s <daglas.aitsen@gmail.com>
2022-11-02 16:08:32 +0200

    working buttons

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from pickle import GLOBAL
 import pygame
 import sys
 pygame.init()
@@ -42,15 +43,21 @@ def draw_text(surf, text, size, x, y, color):
     #pygame.draw.text(text,lefttop=(x,y), width=360, fontname="Boogaloo", fontsize=size,
     #color=color)
 
-def draw_window(score1, score2, turn):
+def draw_window(score1, score2, turn, selected):
     WIN.fill(BLACK)
     shape(50, 50,400,square_gray)
 
     #nupud
     draw_text(WIN,"Select a shape:", 20, 500, 150, WHITE )
-    shape(500, 200 ,40,square_blue)
+    shape(500, 200 ,40,square_gray)
     shape(555, 200 ,40,circle_gray)
     shape(610, 200 ,40,triangle_gray)
+    if selected == square_blue or selected == square_red:
+        shape(500, 200 ,40,selected)
+    elif selected == circle_blue or selected == circle_red:
+        shape(555, 200 ,40,selected)
+    else:
+        shape(610, 200 ,40,selected)
     #square(WIDTH * 0.45, HEIGHT * 0.8,40,square_blue)
     #square(WIDTH * 0.45, HEIGHT * 0.8,40,square_blue)
     draw_text(WIN,f"Player 1:   {score1}", 18, 50, 500, WHITE)
@@ -98,70 +105,44 @@ def game_over_screen(score1, score2):
             if event.type == pygame.KEYUP:
                 waiting = False
 
-def user_click():
-    global FIELD_WIDTH, FIELD_HEIGHT
+def user_click(x,y):
+    global FIELD_WIDTH, FIELD_HEIGHT, turn, selected
 
-    #Salvesta klicki koordinaadid muutujatesse
-    x, y = pygame.mouse.get_pos()
-
-    #if (kordinaadid ehk click on mänguväljal):
-        #if x < FIELD_WIDTH / 10:
-            #col = 1
-        #elif x < FIELD_WIDTH / 10 * 2
-            #col = 2
-        #...
-
-        #if x < FIELD_HEIGHt / 10:
-            #row = 1
-        #elif x < FIELD_HEIGHT / 10 * 2
-            #row = 2
-        #...
-    #elif (koordinaadid on mõnel nupul):
-        #
-    #else:
-        #return
-  
-    # Siin saaks WIDTHiks võtta mänguvälja widthi ja siis jagada columnite arvuga ehk siis meie juhul 10
-    # get column of mouse click (1-3)
-    """  
-    if(x<WIDTH / 3):
-        col = 1   
-    elif (x<WIDTH / 3 * 2):
-        col = 2   
-    elif(x<WIDTH):
-        col = 3 
-    else:
-        col = None """
-  
-    # get row of mouse click (1-3)
-    """ if(y<HEIGHT / 3):
-        row = 1
-     
-    elif (y<HEIGHT / 3 * 2):
-        row = 2
-     
-    elif(y<HEIGHT):
-        row = 3
-     
-    else:
-        row = None """
-       
-
-    #Vaatab, kas row ja col pole none ning siis vaatab, kas järjendis vastav väli on tühi ja siis lisab sinna
-    """ if(row and col and board[row-1][col-1] is None):
-        global XO
- 
-        drawXO(row, col)
-        check_win() """
-    
-
+    #See selectib vastava kuju ja annab sellele värvi, vaadates, kelle kord on hetkel
+    if x >= 500 and x <=540 and y >= 200 and y <=240:
+        if turn%2:
+            selected = square_blue
+        else:
+            selected = square_red
+        print("ruut")
+    elif x >= 555 and x <=595 and y >= 200 and y <=240:
+        if turn%2:
+            selected = circle_blue
+        else:
+            selected = circle_red
+        print("ring")
+    elif x >= 610 and x <=650 and y >= 200 and y <=240:
+        if turn%2:
+            selected = triangle_blue
+        else:
+            selected = triangle_red
+        print("kolmnurk")
+    if x >= 50 and x <=450 and y >= 50 and y <=450:
+        turn += 1
+        if turn%2:
+            selected = square_blue
+        else:
+            selected = square_red
+        print("turn canged")
+turn = 1
+score1 = 0
+score2 = 30
+selected = square_blue
 def main():
+    global turn, selected, score1, score2
     running = True
     #game_start = True
     game_over = False
-    turn = 1
-    score1 = 0
-    score2 = 30
     while running:
         clock.tick(FPS)
         """ if game_start:
@@ -176,36 +157,31 @@ def main():
             score2 = 0 
 
         #See funktsioon uuendab pilti
-        draw_window(score1, score2, turn)
+        draw_window(score1, score2, turn, selected)
         if turn%2:
             score1 += 1
         else: 
             score2 += 1
-        turn += 1
+        #turn += 1
 
         waiting = True
         while waiting:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    #running = False
+                    running = False
+                    waiting = False
                     pygame.quit()
                     sys.exit()
                 if event.type == pygame.KEYUP:
                     #user_click()
                     waiting = False
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    # Set the x, y postions of the mouse click
                     x, y = event.pos
-                    # siin vaata iga shape'i kas click on sinna
-                    #if square_blue.get_rect().collidepoint(x, y):
-                        #print(x)
-
-                    #See hard-coding esimese nupu näiteks, mis kui mõelda ongi suht parim variant, lihtsalt pane lõppu or... ja kontrolli teist kahte nuppu ka
-                    if x >= 500 and x <=540 and y >= 200 and y <=240:
-                        print(x)
+                    user_click(x,y)
+                    waiting = False
         #See on tingimus, mis lõpetab mängu
-        if score1 >= 5:
-            game_over = True
+        """ if score1 >= 5:
+            game_over = True """
     pygame.quit()
     sys.exit()
 
